700225cca9c3378d04c9f42e425c7cb05c1c54fb
Diogo Marcos <contato@diogomarcos.com>
2017-10-16 10:48:58 -0200

    Criando o app.js para a execução da aplicação

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const routesApi = require('./src/routes/api');
+const configSystem = require('./src/config/system');
+
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(routesApi);
+
+app.use('/public', express.static(path.join(__dirname, 'src/public')));
+
+app.get('*', function(req, res) {
+    res.status(404)
+       .render('error', {
+            title: configSystem.NAME_APP,
+            copyright: configSystem.COPYRIGHT_APP,
+            author: configSystem.AUTHOR_APP,
+            status: '404 Not Found',
+            message: 'Sorry, an error has occured, Requested page not found!'
+        });
+});
+
+app.use(function(err, req, res, next) {
+    res.status(500)
+       .render('error', {
+            title: configSystem.NAME_APP,
+            copyright: configSystem.COPYRIGHT_APP,
+            author: configSystem.AUTHOR_APP,
+            status: '500 Internal Server Error',
+            message: 'Something wrong happens!'
+        });
+});
+
+app.listen(3000, function() {
+    console.log('Application started');
+});
