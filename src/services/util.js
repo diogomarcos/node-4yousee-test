c4ef9a2730f9ce6b53a1185eb5fdcbfa3cdbb1b8
Diogo Marcos <contato@diogomarcos.com>
2017-10-16 10:47:09 -0200

    Criando os serviços uteis para a aplicação

---
+++ src/services/util.js
@@ -0,0 +1,15 @@
+module.exports = {
+    removeDuplicate: function(arr, prop) {
+        var new_arr = [];
+        var lookup = {};
+
+        for (var i in arr) {
+            lookup[arr[i][prop]] = arr[i];
+        }
+        for (i in lookup) {
+            new_arr.push(lookup[i]);
+        }
+
+        return new_arr;
+    }
+};
